6ce8c3756d508b153997ba2b5f126fab1c80c8d3
Trafalgar <masyyaf@gmail.com>
2020-11-26 15:19:59 -0500

    Update childAPI.java

--- services/src/main/java/com/webapi/API/childAPI.java
+++ services/src/main/java/com/webapi/API/childAPI.java
@@ -1,5 +1,10 @@
 package com.webapi.API;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +18,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.json.JSONObject;
+
 import com.google.gson.Gson;
 import com.webapi.models.Personne;
 import com.webapi.models.Response;
@@ -20,20 +27,39 @@ import com.webapi.services.ChildService;
 import com.webapi.services.UserService;
 
 @Path("/child")
-@Consumes(MediaType.TEXT_PLAIN)
+@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class childAPI {
 
 	@POST
 	@Path("/ChildcourseInscription")
 	
-	public boolean ChildcourseInscription(
-			@PathParam("childFname") String childFname,
-			@PathParam("courseLevel") String courseLevel
-			) {
-		ChildService cs = new ChildService(childFname);
+	public String ChildcourseInscription(InputStream incomingData){
+		BufferedReader streamReader = null;
+		try {
+			streamReader = new BufferedReader(new InputStreamReader(incomingData, "UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		StringBuilder responseStrBuilder = new StringBuilder();
+
+		String inputStr;
+		try {
+			while ((inputStr = streamReader.readLine()) != null)
+			    responseStrBuilder.append(inputStr);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JSONObject inscription =new JSONObject(responseStrBuilder.toString());
+		System.out.println("printing data : " + inscription.toString());
 		
-		return cs.ChildcourseInscription(courseLevel);
+		ChildService us = new ChildService(inscription.getJSONObject("inscription").getString("childFname"));
+		us.ChildcourseInscription(inscription.getJSONObject("inscription").getString("course_code"));
+		
+		
+		return new Gson().toJson(true);
 	}
 	
 	@GET
